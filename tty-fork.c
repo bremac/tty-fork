6aa368414363e1a39806dfdbb1740bc5599f15f6
bremac <bremac@lenovo.localdomain>
2010-01-23 01:07:40 -0500

    More cleanup. Should be roughly complete.

--- tty-fork.c
+++ tty-fork.c
@@ -6,26 +6,50 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 #include "watch.h"
 #include "util.h"
 
-// Terminate when the child process terminates.
-void sig_child(int signal)
+// Maintain these variables globally so that we can gracefully exit when
+// we receive SIGCHLD. Otherwise, a FORCE'd invariant could trigger before
+// the cleanup code is reached, causing the code to apparently exit with
+// an error.
+const char *socket_path = NULL;
+int         socket_fd   = -1;
+int         pty_fd      = -1;
+
+void cleanup()
+{
+    close(pty_fd);
+    close(socket_fd);    // Ensure we can unlink the socket.
+    unlink(socket_path); // Prevent invalid dangling files.
+}
+
+void sigchld(int s)
 {
     exit(0);
 }
 
-void forkpty(int pty, int argc, char **args)
+int forkpty(int argc, char **args)
 {
-    char *ptyname = ptsname(pty);
-    int child;
-    int pts;
+    int pty;
+    char *ptyname;
     
-    FORCE(ptyname, "Could not open a pseudo-terminal slave.");
-    pts = open(ptyname, O_RDWR);
-    FORCE(pts, "Could not access the terminal slave.");
+    FORCE(isatty(0) && isatty(1), "Must call tty-fork from a valid tty.");
+    
+    pty = posix_openpt(O_RDWR | O_NOCTTY);
+    FORCE(pty != -1, "Unable to open a new pseudo-terminal.");
+    
+    FORCE(grantpt(pty) && unlockpt(pty), "Unable to release pseudo-terminal.");
+   
+    if(fork() == 0) {
+        ptyname = ptsname(pty); 
+        FORCE(ptyname != NULL, "Could not open a pseudo-terminal slave.");
+    
+        pty = open(ptyname, O_RDWR);
+        FORCE(pty, "Could not access the terminal slave.");
 
-    if((child = fork()) == 0) {
         // This is the child process. Clone the arguments and call execvp.
         char **argv = malloc(sizeof(char*) * (argc + 1));
 
@@ -33,12 +57,14 @@ void forkpty(int pty, int argc, char **args)
         memmove(argv, args, sizeof(char*) * argc);
         argv[argc] = NULL; // Terminate the vector of arguments.
         
-        dup2(pts, 0);      // Replace stdin/stdout with the pseudo-tty.
-        dup2(pts, 1);
+        dup2(pty, 0);      // Replace stdin/stdout with the pseudo-tty.
+        dup2(pty, 1);
          
         execvp(argv[0], argv);
         FORCE(0, "Unable to execute slave process.");
-    } else signal(SIGCHLD, sig_child);
+    } else signal(SIGCHLD, sigchld);
+
+    return pty;
 }
 
 // The length of the buffer to read data into.
@@ -61,7 +87,7 @@ void select_loop(int pty, int sock)
         FORCE(!FD_ISSET(pty, &watcher->error_set),  "Terminal connection lost.");
         
         if (FD_ISSET(sock, &watcher->read_set)) {
-            new_fd = accept(sock);
+            new_fd = accept(sock, NULL, NULL);
             watch_fd(watcher, new_fd);
             FD_CLR(sock, &watcher->read_set);
             count--;
@@ -80,18 +106,35 @@ void select_loop(int pty, int sock)
     }
 }
 
+int make_domain_socket(const char *path)
+{
+    int sock, retval;
+    struct sockaddr_un addr;
+
+    sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    FORCE(sock != -1, "Unable to create socket for IPC.");
+
+    strncpy(addr.sun_path, path, sizeof(addr.sun_path));
+    retval = bind(sock, (struct sockaddr *)&addr, strlen(path) + sizeof(addr.sun_family));
+    FORCE(retval, "Unable to bind the IPC socket to the filesystem.");
+}
+
+const char *USAGE = "Usage: tty-fork <path> <command> [arguments] ...";
+
 int main(int argc, char **argv)
 {
-    int pty, socket;
+    if (argc < 2)
+        puts(USAGE);
 
-    FORCE(isatty(0) && isatty(1), "Must call tty-fork from a valid tty.");
-    pty = posix_openpt(O_RDWR | O_NOCTTY);
-    FORCE(pty != -1, "Unable to open a new pseudo-terminal.");
-    FORCE(grantpt(pty) && unlockpt(pty), "Unable to release pseudo-terminal.");
+    socket_path = argv[1];
+    socket_fd   = make_domain_socket(argv[1]);
+    
+    pty_fd      = forkpty(argc - 2, argv + 2); // Skip the command and socket path.
 
-    // XXX: Create the UNIX Domain socket for communication.
-    
-    select_loop(pty, socket);
-    
+    atexit(cleanup);
+     
+    select_loop(pty_fd, socket_fd);
+    // XXX: Should not be reached.
+
     return EXIT_SUCCESS;
 }
